6c73a59406e7c7e70f13439da70dbaec7e84ac63
Akile  Nisa Öksüz <116032825+Akile-Nisa-Oksuz@users.noreply.github.com>
2024-10-10 22:40:58 +0300

    Ders 5 - Döngülerle Pratik Uygulamalar, Yıldızlarla Şekil Çizme

---
+++ 05_LoopsWhitStars/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWhitStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region alt alta on yıldız oluşturma
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region yan yana on yıldız oluşturma
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region altlata ve yan yana 10 yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j < 10; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("*"); 
+            //}
+            #endregion
+
+            #region dik üçgen
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region tersine dik üçgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region yarım baklava
+            //for (int i = 1; i <= 5; i++)
+            //{for (int j = 1; j <= i; j++)
+            //{
+            //        Console.Write("*");
+            //   }
+            //  Console.WriteLine();
+            //}
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region  baklava dilimi
+            for (int i = 0; i <= 5; i++)
+            {
+
+            }
+            #endregion
+            Console.Read();
+        }
+
+    }
+}
