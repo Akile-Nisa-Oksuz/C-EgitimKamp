ba23509f6c2ad559980d7e19da8c7ec1b72f2c08
Akile  Nisa Öksüz <116032825+Akile-Nisa-Oksuz@users.noreply.github.com>
2024-10-28 22:16:02 +0300

    değiştirme

--- 04-MakingDecision/Properties/AssemblyInfo.cs
+++ 04-MakingDecision/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("04-MakingDecision")]
+[assembly: AssemblyTitle("03-MakingDecision")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("04-MakingDecision")]
+[assembly: AssemblyProduct("03-MakingDecision")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
